2a23bee8cbff7468e277bd8e9d568974edcbcd88
greendb16 <greendb16@gmail.com>
2019-06-12 11:32:04 -0400

    First and final

---
+++ src/Math2.java
@@ -0,0 +1,10 @@
+public class Math2 {
+    public static void main(String[]args) {
+
+        int myNumber = 2;
+        double myOtherNumber = 1.7938;
+
+        System.out.println("My Number is " + myNumber);
+        System.out.println("My other number is " + myOtherNumber);
+    }
+}
